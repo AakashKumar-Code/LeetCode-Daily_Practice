acf621e7e8969387d4eeb091e8376b26e6c13ae6
AAKASH KUMAR <aakashkumar27022001@gmail.com>
2024-01-28 16:28:21 +0530

    Time: 3 ms (88.51%) | Memory: 11.6 MB (6.82%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
 
-    int solve(int i, int j, int &n, vector<vector<int>>&arr, vector<vector<int>>&dp){
-        if(i==n) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int d=arr[i][j]+solve(i+1, j, n, arr, dp);
-        int dr=arr[i][j]+solve(i+1, j+1, n, arr, dp);
-        return dp[i][j]=min(d, dr);
-    }
-
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-        return solve(0, 0, n, triangle, dp);
+        vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
+        for(int j=0; j<n; j++){
+            dp[n-1][j]=triangle[n-1][j];
+        }
+        for(int i=n-2; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                int d=triangle[i][j]+dp[i+1][j];
+                int dr=triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(d, dr);
+            }
+        }
+        return dp[0][0];
     }
 };
