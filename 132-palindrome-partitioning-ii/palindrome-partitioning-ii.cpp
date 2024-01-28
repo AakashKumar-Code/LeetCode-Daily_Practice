b23ef0d0d386f13315b92ca526101508ea06ff23
AAKASH KUMAR <aakashkumar27022001@gmail.com>
2024-01-28 13:36:13 +0530

    Time: 1762 ms (12.93%) | Memory: 7.6 MB (55.90%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
 
-
     bool isPal(int lo, int hi, string &s){
         while(lo<=hi){
             if(s[lo++]!=s[hi--]) return 0;
@@ -9,22 +8,19 @@ public:
         return 1;
     }
 
-    int solve(int ind, int &n, string &s, vector<int>&dp){
-        if(ind==n) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int mini=1e9;
-        for(int i=ind; i<n; i++){
-            if(isPal(ind, i, s)){
-                int ops=1+solve(i+1, n, s, dp);
-                mini=min(mini, ops);
-            }
-        }
-        return dp[ind]=mini;
-    }
-
     int minCut(string s) {
         int n=s.length();
-        vector<int>dp(n+1, -1);
-        return solve(0, n, s, dp)-1;
+        vector<int>dp(n+1, 0);
+        for(int ind=n-1; ind>=0; ind--){
+            int mini=1e9;
+            for(int i=ind; i<n; i++){
+                if(isPal(ind, i, s)){
+                    int ops=1+dp[i+1];
+                    mini=min(mini, ops);
+                }
+            }
+            dp[ind]=mini;
+        }
+        return dp[0]-1;
     }
 };
