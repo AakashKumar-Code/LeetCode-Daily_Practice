016718fede76545cdb3f37e7124db1d4516c0860
AAKASH KUMAR <aakashkumar27022001@gmail.com>
2024-01-28 15:57:10 +0530

    Time: 7 ms (93.82%) | Memory: 10.8 MB (15.36%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
 
-    int solve(int i, int n, int &k, vector<int>&arr, vector<int>&dp){
-        if(i==n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int maxi=-1e9;
-        int maxSum=-1e9;
-        int len=0;
-        for(int p=i; p<min(k+i, n); p++){
-            maxi=max(maxi, arr[p]);
-            len++;
-            int ops=maxi*len+solve(p+1, n, k, arr, dp);
-            maxSum=max(maxSum, ops);
-        }
-        return dp[i]=maxSum;
-    }
-
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
-        vector<int>dp(n+1, -1);
-        return solve(0, n, k, arr, dp);
+        vector<int>dp(n+1, 0);
+        for(int i=n-1; i>=0; i--){
+            int maxi=-1e9;
+            int maxSum=-1e9;
+            int len=0;
+            for(int p=i; p<min(k+i, n); p++){
+                maxi=max(maxi, arr[p]);
+                len++;
+                int ops=maxi*len+dp[p+1];
+                maxSum=max(maxSum, ops);
+            }
+            dp[i]=maxSum;
+        }
+        return dp[0];
     }
 };
