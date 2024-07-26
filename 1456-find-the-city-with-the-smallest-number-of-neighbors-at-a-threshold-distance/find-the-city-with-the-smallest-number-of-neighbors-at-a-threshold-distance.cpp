b63e5a3fca0ed5cb44017873f4d4161ebec246e8
AAKASH KUMAR <aakashkumar27022001@gmail.com>
2024-07-26 23:59:15 +0530

    Time: 24 ms (74.13%) | Memory: 15.5 MB (75.48%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>>arr(n, vector<int>(n, 1e9));
 
-    void bellman_ford(int src, int &n, vector<vector<int>>&e, vector<int>&dist){
-        dist[src]=0;
+        for(auto &it:edges){
+            int u=it[0];
+            int v=it[1];
+            int wt=it[2];
 
-        for(int k=1; k<=n-1; k++){
-            for(auto &it:e){
-                int u=it[0];
-                int v=it[1];
-                int wt=it[2];
+            arr[u][v]=wt;
+            arr[v][u]=wt;
+        }
 
-                if(dist[u]!=INT_MAX && dist[u]+wt<dist[v]){
-                    dist[v]=dist[u]+wt;
-                }
-                if(dist[v]!=INT_MAX && dist[v]+wt<dist[u]){
-                    dist[u]=dist[v]+wt;
+        for(int i=0; i<n; i++){
+            arr[i][i]=0;
+        }
+
+        for(int via=0; via<n; via++){
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    
+                    arr[i][j]=min(arr[i][j], arr[i][via]+arr[via][j]);
                 }
             }
         }
-    }
 
-    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>>mat(n, vector<int>(n, INT_MAX));
-
-        for(int i=0; i<n; i++){
-            bellman_ford(i, n, edges, mat[i]);
-        }
-
-        int ans=-1, mini=INT_MAX;
+        int ans=-1, mini=1e9;
 
         for(int i=0; i<n; i++){
             int cnt=0;
             for(int j=0; j<n; j++){
-                if(i!=j && mat[i][j]<=distanceThreshold){
-                    cnt++;
-                }
+                if(i!=j && arr[i][j]<=distanceThreshold) cnt++;
             }
             if(mini>=cnt){
                 mini=cnt;
@@ -42,6 +38,5 @@ public:
             }
         }
         return ans;
-
     }
 };
