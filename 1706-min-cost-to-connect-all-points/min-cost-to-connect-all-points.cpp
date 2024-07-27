d5a79a91b30aec7a264e7cd830d8f7f5388b8477
AAKASH KUMAR <aakashkumar27022001@gmail.com>
2024-07-27 23:41:15 +0530

    Time: 1869 ms (5.01%) | Memory: 344.5 MB (7.12%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,12 +1,34 @@
 class Solution {
 public:
 
-    #define P pair<int, int>
+    
+    int find(int x, vector<int>&par){
+        if(x==par[x]){
+            return x;
+        }
+        return par[x]=find(par[x], par);
+    }
+    
+    void Union(int x, int y, vector<int>&par, vector<int>&rank){
+        int par_x=find(x, par);
+        int par_y=find(y, par);
+        
+        if(par_x==par_y) return;
+        
+        if(rank[par_x]>rank[par_y]){
+            par[par_y]=par_x;
+        }else if(rank[par_x]<rank[par_y]){
+            par[par_x]=par_y;
+        }else{
+            par[par_x]=par_y;
+            rank[par_y]++;
+        }
+    }
 
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n=points.size();
 
-        vector<vector<int>>adj[n];
+        vector<vector<int>>vec;
 
         for(int i=0; i<n; i++){
             int x1=points[i][0];
@@ -17,34 +39,27 @@ public:
                 int y2=points[j][1];
 
                 int wt=abs(x2-x1)+abs(y2-y1);
-                adj[i].push_back({j, wt});
-                
+                vec.push_back({wt, i, j});                
             }
         }
+        sort(vec.begin(), vec.end());
 
-        priority_queue< P, vector<P>, greater<P> >pq;
         int ans=0;
-
-        pq.push({0, 0});
-        vector<bool>vis(n, 0);
-
-        while(!pq.empty()){
-            auto it=pq.top();
-            pq.pop();
-
-            int node=it.second;
-            int wt=it.first;
-
-            if(vis[node]) continue;
-            else{
-                vis[node]=1;
+        vector<int>par(n), rank(n, 0);
+        
+        for(int i=0; i<n; i++) par[i]=i;
+        
+        for(auto &it:vec){
+            int u=it[1];
+            int v=it[2];
+            int wt=it[0];
+            
+            int par_u=find(u, par);
+            int par_v=find(v, par);
+            
+            if(par_u!=par_v){
                 ans+=wt;
-            }
-
-            for(auto it:adj[node]){
-                if(!vis[it[0]]){
-                    pq.push({it[1], it[0]});
-                }
+                Union(u, v, par, rank);
             }
         }
         return ans;
