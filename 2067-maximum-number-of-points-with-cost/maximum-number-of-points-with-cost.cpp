41b3c1e29faca6dce23b9e5345d470ea5ee3b929
AAKASH KUMAR <aakashkumar27022001@gmail.com>
2024-08-17 14:49:05 +0530

    Time: 183 ms (69.21%) | Memory: 101.7 MB (69.75%) - LeetSync

--- 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.cpp
+++ 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.cpp
@@ -4,22 +4,22 @@ public:
         int n=points.size();
         int m=points[0].size();
 
-        vector<long long>curr(m), prev(m);
+        vector<long long>curr(m, 0), prev(m);
 
         for(int j=0; j<m; j++) prev[j]=points[0][j];
 
         for(int i=1; i<n; i++){
-            vector<long long>left(m, 0), right(m, 0);
-            left[0]=prev[0];
+            vector<long long>right(m, 0);
+            curr[0]=prev[0];
             for(int j=1; j<m; j++){
-                left[j]=max(left[j-1]-1, prev[j]);
+                curr[j]=max(curr[j-1]-1, prev[j]);
             }
             right[m-1]=prev[m-1];
             for(int j=m-2; j>=0; j--){
                 right[j]=max(right[j+1]-1, prev[j]);
             }
             for(int j=0; j<m; j++){
-                curr[j]=max(left[j], right[j])+points[i][j];
+                curr[j]=max(curr[j], right[j])+points[i][j];
             }
             prev=curr;
         }
