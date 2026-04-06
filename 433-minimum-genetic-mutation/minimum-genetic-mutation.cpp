d066224f05096cf1a6c6860f61808b911e2c197f
AAKASH KUMAR <aakashkumar27022001@gmail.com>
2026-04-06 17:59:00 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (43.52%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
     int minMutation(string startGene, string endGene, vector<string>& bank) {
-        int ans=0;
-        int n=bank.size();
-        vector<bool>vis(n, 0);
-        int m=startGene.length();
-
-        if(n==0) return -1;
+        unordered_set<string>b={bank.begin(), bank.end()};
+        if(b.find(endGene)==b.end()) return -1;
+        if (startGene == endGene) return 0;
+        int steps=0;
+        unordered_set<string>vis;
+        vector<char>change{'A','G', 'T', 'C'};
 
         queue<string>q;
-
+        vis.insert(startGene);
         q.push(startGene);
 
         while(!q.empty()){
-            int sz=q.size();
-            ans++;
-            for(int t=0; t<sz; t++){
-                string node=q.front();
-                q.pop();
-                
-                if(node==endGene) return ans-1;
+          int sz=q.size();
+          for(int i=0; i<sz; i++){
+            string node=q.front();
+            q.pop();
 
-                for(int i=0; i<n; i++){
-                    if(vis[i]) continue;
-                    int diff=0;
-                    for(int j=0; j<m; j++){
-                        if(node[j]!=bank[i][j]) diff++;
-                    }
-                    if(diff==1){
-                        vis[i]=1;
-                        q.push(bank[i]);
-                    }
-                }                
+            if(node==endGene) return steps;
+
+            for(int x=0; x<node.length(); x++){
+              char org=node[x];
+              for(int t=0; t<4; t++){
+                char ch=change[t];
+                if(org==ch) continue;
+                string tmp=node;
+                tmp[x]=ch;
+                if(b.find(tmp)!=b.end() && vis.find(tmp)==vis.end()){
+                  vis.insert(tmp);
+                  q.push(tmp);                  
+                }
+              }
             }
+          }
+          steps++;
         }
+
         return -1;
-        
     }
 };
