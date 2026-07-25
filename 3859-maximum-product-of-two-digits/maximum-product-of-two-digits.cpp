cbc7616fbae93cbb2b5182f09ec681c158695d77
AAKASH KUMAR <aakashkumar27022001@gmail.com>
2026-07-25 11:16:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (59.34%) - LeetSync

---
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int max1=0, max2=0;
+        while(n){
+            int d=n%10;
+            n/=10;
+            if(d>=max1){
+                max2=max1;
+                max1=d;
+            }else if(d>max2){
+                max2=d;
+            }
+        }
+
+        return max1*max2;        
+    }
+};
